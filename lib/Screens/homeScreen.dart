9a5c6dc5d36df0d0b6e45178eb5d0e07debe1842
phillippZZ <g9zhu@uwaterloo.ca>
2025-04-04 10:05:38 -0400

    Update homeScreen.dart

--- lib/Screens/homeScreen.dart
+++ lib/Screens/homeScreen.dart
@@ -1,200 +1,102 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:flutter_weather/screens/locationError.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:material_floating_search_bar_2/material_floating_search_bar_2.dart';
-import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_weather/provider/weatherProvider.dart';
+import 'package:flutter_weather/widgets/WeatherInfoHeader.dart';
+import 'package:flutter_weather/widgets/mainWeatherDetail.dart';
+import 'package:flutter_weather/widgets/mainWeatherInfo.dart';
+import 'package:flutter_weather/widgets/sevenDayForecast.dart';
+import 'package:flutter_weather/widgets/twentyFourHourForecast.dart';
 
-import '../provider/weatherProvider.dart';
-import '../theme/colors.dart';
-import '../theme/textStyle.dart';
-import '../widgets/WeatherInfoHeader.dart';
-import '../widgets/mainWeatherDetail.dart';
-import '../widgets/mainWeatherInfo.dart';
-import '../widgets/sevenDayForecast.dart';
-import '../widgets/twentyFourHourForecast.dart';
+class WeatherWidget extends StatefulWidget {
+  final double width;
+  final double height;
 
-import 'requestError.dart';
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const WeatherWidget({
+    super.key,
+    required this.width,
+    required this.height,
+  });
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<WeatherWidget> createState() => _WeatherWidgetState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  FloatingSearchBarController fsc = FloatingSearchBarController();
-
+class _WeatherWidgetState extends State<WeatherWidget> {
   @override
   void initState() {
     super.initState();
-    requestWeather();
-  }
-
-  Future<void> requestWeather() async {
-    await Provider.of<WeatherProvider>(context, listen: false)
-        .getWeatherData(context);
+    // Request weather data when widget initializes
+    Future.microtask(() => 
+      Provider.of<WeatherProvider>(context, listen: false)
+          .getWeatherData(context)
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Consumer<WeatherProvider>(
-        builder: (context, weatherProv, _) {
-          if (!weatherProv.isLoading && !weatherProv.isLocationserviceEnabled)
-            return LocationServiceErrorDisplay();
-
-          if (!weatherProv.isLoading &&
-              weatherProv.locationPermission != LocationPermission.always &&
-              weatherProv.locationPermission != LocationPermission.whileInUse) {
-            return LocationPermissionErrorDisplay();
-          }
-
-          if (weatherProv.isRequestError) return RequestErrorDisplay();
-
-          if (weatherProv.isSearchError) return SearchErrorDisplay(fsc: fsc);
-
-          return Stack(
-            children: [
-              ListView(
-                physics: BouncingScrollPhysics(),
-                padding: const EdgeInsets.all(12.0).copyWith(
-                  top: kToolbarHeight +
-                      MediaQuery.viewPaddingOf(context).top +
-                      24.0,
+    return ChangeNotifierProvider(
+      create: (_) => WeatherProvider(),
+      child: Builder(
+        builder: (context) {
+          return SizedBox(
+            width: widget.width,
+            height: widget.height,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Theme(
+                data: ThemeData(
+                  appBarTheme: const AppBarTheme(
+                    backgroundColor: Colors.white,
+                    iconTheme: IconThemeData(color: Colors.blue),
+                    elevation: 0,
+                  ),
+                  scaffoldBackgroundColor: Colors.white,
+                  primaryColor: Colors.blue,
+                  visualDensity: VisualDensity.adaptivePlatformDensity,
+                  colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.white),
                 ),
-                children: [
-                  WeatherInfoHeader(),
-                  const SizedBox(height: 16.0),
-                  MainWeatherInfo(),
-                  const SizedBox(height: 16.0),
-                  MainWeatherDetail(),
-                  const SizedBox(height: 24.0),
-                  TwentyFourHourForecast(),
-                  const SizedBox(height: 18.0),
-                  SevenDayForecast(),
-                ],
+                child: Scaffold(
+                  body: Consumer<WeatherProvider>(
+                    builder: (context, weatherProv, _) {
+                      if (weatherProv.isLoading) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+
+                      if (weatherProv.isRequestError) {
+                        return const Center(
+                          child: Text('Unable to load weather data'),
+                        );
+                      }
+
+                      return MediaQuery(
+                        data: MediaQuery.of(context).copyWith(
+                          size: Size(widget.width, widget.height),
+                          padding: EdgeInsets.zero,
+                        ),
+                        child: ListView(
+                          physics: const BouncingScrollPhysics(),
+                          padding: const EdgeInsets.all(12.0),
+                          children: const [
+                            WeatherInfoHeader(),
+                            SizedBox(height: 16.0),
+                            MainWeatherInfo(),
+                            SizedBox(height: 16.0),
+                            MainWeatherDetail(),
+                            SizedBox(height: 24.0),
+                            TwentyFourHourForecast(),
+                            SizedBox(height: 18.0),
+                            SevenDayForecast(),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
               ),
-              CustomSearchBar(fsc: fsc),
-            ],
+            ),
           );
         },
       ),
-    );
-  }
-}
-
-class CustomSearchBar extends StatefulWidget {
-  final FloatingSearchBarController fsc;
-  const CustomSearchBar({
-    Key? key,
-    required this.fsc,
-  }) : super(key: key);
-
-  @override
-  State<CustomSearchBar> createState() => _CustomSearchBarState();
-}
-
-class _CustomSearchBarState extends State<CustomSearchBar> {
-  List<String> _citiesSuggestion = [
-    'New York',
-    'Tokyo',
-    'Dubai',
-    'London',
-    'Singapore',
-    'Sydney',
-    'Wellington'
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return FloatingSearchBar(
-      controller: widget.fsc,
-      hint: 'Search...',
-      clearQueryOnClose: false,
-      scrollPadding: const EdgeInsets.only(top: 16.0, bottom: 56.0),
-      transitionDuration: const Duration(milliseconds: 400),
-      borderRadius: BorderRadius.circular(16.0),
-      transitionCurve: Curves.easeInOut,
-      accentColor: primaryBlue,
-      hintStyle: regularText,
-      queryStyle: regularText,
-      physics: const BouncingScrollPhysics(),
-      elevation: 2.0,
-      debounceDelay: const Duration(milliseconds: 500),
-      onQueryChanged: (query) {},
-      onSubmitted: (query) async {
-        widget.fsc.close();
-        await Provider.of<WeatherProvider>(context, listen: false)
-            .searchWeather(query);
-      },
-      transition: CircularFloatingSearchBarTransition(),
-      actions: [
-        FloatingSearchBarAction(
-          showIfOpened: false,
-          child: PhosphorIcon(
-            PhosphorIconsBold.magnifyingGlass,
-            color: primaryBlue,
-          ),
-        ),
-        FloatingSearchBarAction.icon(
-          showIfClosed: false,
-          showIfOpened: true,
-          icon: PhosphorIcon(
-            PhosphorIconsBold.x,
-            color: primaryBlue,
-          ),
-          onTap: () {
-            if (widget.fsc.query.isEmpty) {
-              widget.fsc.close();
-            } else {
-              widget.fsc.clear();
-            }
-          },
-        ),
-      ],
-      builder: (context, transition) {
-        return ClipRRect(
-          borderRadius: BorderRadius.circular(8.0),
-          child: Material(
-            color: Colors.white,
-            elevation: 4.0,
-            child: ListView.separated(
-              shrinkWrap: true,
-              physics: BouncingScrollPhysics(),
-              padding: EdgeInsets.zero,
-              itemCount: _citiesSuggestion.length,
-              itemBuilder: (context, index) {
-                String data = _citiesSuggestion[index];
-                return InkWell(
-                  onTap: () async {
-                    widget.fsc.query = data;
-                    widget.fsc.close();
-                    await Provider.of<WeatherProvider>(context, listen: false)
-                        .searchWeather(data);
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.all(22.0),
-                    child: Row(
-                      children: [
-                        PhosphorIcon(PhosphorIconsFill.mapPin),
-                        const SizedBox(width: 22.0),
-                        Text(data, style: mediumText),
-                      ],
-                    ),
-                  ),
-                );
-              },
-              separatorBuilder: (context, index) => Divider(
-                thickness: 1.0,
-                height: 0.0,
-              ),
-            ),
-          ),
-        );
-      },
     );
   }
 }
